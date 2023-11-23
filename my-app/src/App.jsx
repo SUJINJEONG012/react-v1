70f91f9eb9e408dfb9783055b09dfbaabafa1cce
angela <peekaboo32@naver.com>
2023-11-23 17:06:04 +0900

    컴포넌트로 색상 따로 분리

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,19 +1,22 @@
+import { ColoredMessage } from "./components/ColoredMessage";
+
 //컴포넌트의 확장자는 .jsx로 사용할 것을 권장 
 export const App = () => {
     // 버튼 클릭시 실행되는 함수 정의
     const onClickButton = () => {
         alert();
     }
-    //css 객체
-    const contentStyle ={
-        color:"blue",
+    const contentPinkStyle={
+        color:"pink",
         fontSize: "20px"
     }
+   
     return  (
         <>
         {console.log("test")}
                     <h1 style={{color:"red"}}>안녕하세요.</h1>
-            <p style={contentStyle}>잘 지내시죠?</p>
+         <ColoredMessage />
+            <p style={contentPinkStyle}>잘 지냅니다.</p>
            <button onClick={onClickButton}>버튼</button>
         </>
     );
---
+++ my-app/src/components/ColoredMessage.jsx
@@ -0,0 +1,8 @@
+export const ColoredMessage = ()=> {
+    const contentStyle ={
+        color:"blue",
+        fontSize: "20px"
+    };
+
+    return <p style={contentStyle}>잘 지내시죠?</p>
+};
