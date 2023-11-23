12144c86c92f5fee363925821bf96d6eba0c4b2b
angela <peekaboo32@naver.com>
2023-11-23 15:31:46 +0900

    컴포넌트 파일의 확장자는 .jsx로 표시

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,10 @@
+//컴포넌트의 확장자는 .jsx로 사용할 것을 권장 
 export const App = () => {
     return  (
         <>
             <h1>안녕하세요.</h1>
             <p>잘 지내시죠?</p>
+            <button>버튼</button>
         </>
     );
 };
