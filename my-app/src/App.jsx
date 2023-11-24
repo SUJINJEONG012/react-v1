50ec457dddf62198ddaa4b3ba6d420c82f01eb67
SUJINJEONG012 <peekaboo32@naver.com>
2023-11-24 21:39:38 +0900

    useState() 함수 사용해보기

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
-
 //컴포넌트의 확장자는 .jsx로 사용할 것을 권장 
 export const App = () => {
+    //State 정의
+    const[num, setNum] = useState(0);
+
     // 버튼 클릭시 실행되는 함수 정의
     const onClickButton = () => {
-        alert();
+       setNum(num+1);
     }
    
     return  (
@@ -14,6 +17,7 @@ export const App = () => {
         <ColoredMessage color="blue">잘 지내시죠 ?</ColoredMessage>
         <ColoredMessage color="pink">잘지냅니다 ! !</ColoredMessage>
            <button onClick={onClickButton}>버튼</button>
-        </>
+           <p>{num}</p>     
+         </>
     );
 };
