232c1fd7b5c9af00fcebf692f09ed55460273865
SUJINJEONG012 <peekaboo32@naver.com>
2023-11-24 20:36:04 +0900

    pink로 색상변경해서 잘지냅니다 문구도 변경

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,17 +6,12 @@ export const App = () => {
     const onClickButton = () => {
         alert();
     }
-    const contentPinkStyle={
-        color:"pink",
-        fontSize: "20px"
-    }
    
     return  (
         <>
-       
-                    <h1 style={{color:"red"}}>안녕하세요.</h1>
-         <ColoredMessage color="blue" message="잘지내시죠 zz?"/>
-            <p style={contentPinkStyle}>잘 지냅니다.</p>
+        <h1 style={{color:"red"}}>안녕하세요.</h1>
+        <ColoredMessage color="blue" message="잘지내시죠 zz?"/>
+        <ColoredMessage color="pink" message="잘지냅니다!!!"/>
            <button onClick={onClickButton}>버튼</button>
         </>
     );
