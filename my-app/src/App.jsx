4da5fc28fe622838e936d0bf2f4201a86bf5f5cd
SUJINJEONG012 <peekaboo32@naver.com>
2023-11-25 11:43:33 +0900

    node-sass 설치안됌

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import { ColoredMessage } from "./components/ColoredMessage";
+import { CssModules } from "./components/CssModules";
 //컴포넌트의 확장자는 .jsx로 사용할 것을 권장 
 export const App = () => {
+
+    console.log("랜더링 ");
+    
+   
     //State 정의
     const[num, setNum] = useState(0);
+
+    useEffect(()=>{
+        alert();
+    }, [num]);
 
     // 버튼 클릭시 실행되는 함수 정의
     const onClickButton = () => {
@@ -17,7 +27,8 @@ export const App = () => {
         <ColoredMessage color="blue">잘 지내시죠 ?</ColoredMessage>
         <ColoredMessage color="pink">잘지냅니다 ! !</ColoredMessage>
            <button onClick={onClickButton}>버튼</button>
-           <p>{num}</p>     
+           <p>{num}</p>  
+         <CssModules/>     
          </>
     );
 };
---
+++ my-app/src/components/CssModules.jsx
@@ -0,0 +1,8 @@
+export const CssModules = () => {
+    return (
+        <div>
+            <p>Css Modules입니다.</p>
+            <button>버튼</button>
+        </div>
+    );
+};
