fc29d98782aa833587f8edd5f569d0fd4f81cdce
SUJINJEONG012 <peekaboo32@naver.com>
2023-11-26 01:00:46 +0900

    src파일 다 삭제 후 index.js 다시생성

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { ReactDOM } from "react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const App= () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ return <h1>안녕하세요</h1>;
+}
+
+ReactDOM.render(<App/>, document.getElementById("root"));
