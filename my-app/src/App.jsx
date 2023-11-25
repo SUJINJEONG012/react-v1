47b86b728ae7ce557f449cde55b8dd6b0ded5bc0
SUJINJEONG012 <peekaboo32@naver.com>
2023-11-26 02:17:33 +0900

    버전변경으로 인해 다시 설정  후 커밋

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,14 @@
 export const App= () => {
+  //버튼 클릭시 실행되는 함수 정의
+  const onClickButton = ()=>{
+    alert();
+  }
     return (
        <>
+    {console.log("테스트")}
        <h1>안녕하세요</h1>
        <p> 잘지내시죠?</p>
+       <button onClick={onClickButton}>버튼</button>
        </>
        );
     };
