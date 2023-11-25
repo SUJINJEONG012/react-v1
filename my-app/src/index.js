c5b6756efaaf49586ecba0bb34d8b013eb7b098b
SUJINJEONG012 <peekaboo32@naver.com>
2023-11-26 01:13:02 +0900

    fragment, <> 빈태그 사용 적용법

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,13 @@
-import React, { ReactDOM } from "react";
+import React, { Fragment, ReactDOM } from "react";
 
 const App= () => {
 
- return <h1>안녕하세요</h1>;
-}
+ return (
+    <>
+    <h1>안녕하세요</h1>
+    <p> 잘지내시죠?</p>
+    </>
+    );
+ };
 
 ReactDOM.render(<App/>, document.getElementById("root"));
