8d244d3b720dcca142e6f36b0121ce24f01b3e91
SUJINJEONG012 <peekaboo32@naver.com>
2023-11-26 17:38:57 +0900

    &:hover 적용

--- my-app/src/components/CssModules.jsx
+++ my-app/src/components/CssModules.jsx
@@ -1,10 +1,10 @@
-import classes from './CssModules/Modules.scss';
+import classes from './CssModules.module.scss';
 
 export const CssModules = () =>{
     return(
     <div className={classes.container}>
         <p className={classes.title}>css Modules 입니다.</p>
-        <button>버튼</button>
+        <button className={classes.button}>버튼</button>
     </div>
     );
 };
