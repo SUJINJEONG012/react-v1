da8a0f0b13fa70986d1a3c436166678547282aeb
SUJINJEONG012 <peekaboo32@naver.com>
2023-11-26 14:43:15 +0900

    css 객체만들어서 스타일 적용

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,11 +3,23 @@ export const App= () => {
   const onClickButton = ()=>{
     alert();
   }
+
+  //css 객체
+  const contentStyle = {
+    color:"blue",
+    fontSize:"20px"
+  }
+  const contentPinkStyle ={
+    color:"pink",
+    fontSize:"20px"
+  }
+
     return (
        <>
     {console.log("테스트")}
-       <h1>안녕하세요</h1>
-       <p> 잘지내시죠?</p>
+       <h1 style={{color:"red"}}>안녕하세요</h1>
+       <p style={contentStyle}> 잘지내시죠?</p>
+       <p style={contentPinkStyle}>잘 지냅니다.</p>
        <button onClick={onClickButton}>버튼</button>
        </>
        );
